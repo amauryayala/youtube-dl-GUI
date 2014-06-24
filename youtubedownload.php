2f4ede98875f09c829766e4ff8d4dca5814af3d1
Amaury <amauryayala@users.noreply.github.com>
2014-06-24 16:32:24 -0400

    Create youtubedownload.php

---
+++ youtubedownload.php
@@ -0,0 +1,43 @@
+<?php
+class youtubedownload{
+		private $_url = null,$_name = null,$_downloadDir = "/mount/network/youtube/",$_errors = array();
+		public function __construct(){
+		}
+		public function download($url){
+			if($url){
+				chdir($this->_downloadDir);
+				if( ($fp = popen("youtube-dl  $url", "r")) ) {
+					echo "<pre>";
+					while( !feof($fp) ){
+						echo fread($fp, 1024);
+						flush(); // you have to flush buffer
+					}
+					echo "</pre>";
+				fclose($fp);
+				}
+			}else{
+				$this->addError("I need an url!");
+			}
+		}
+		public function downloadmp3($url){
+			if($url){
+				chdir($this->_downloadDir);
+				if( ($fp = popen("youtube-dl --extract-audio --audio-format=mp3 '$url'", "r")) ) {
+					echo "<pre>";
+					while( !feof($fp) ){
+						echo fread($fp, 1024);
+						flush(); // you have to flush buffer
+					}
+					echo "</pre>";
+				fclose($fp);
+				}
+			}
+		}
+		private function addError($error){
+			$this->_errors[] = $error;
+		}
+		public function errors(){
+			return $this->_errors;
+		}
+	}
+	?>
